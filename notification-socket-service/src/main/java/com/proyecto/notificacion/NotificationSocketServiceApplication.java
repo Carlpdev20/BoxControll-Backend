1816327544bf794a2c199b698500a79581794757
Carlpdev20 <cperez.16j@gmail.com>
2026-06-02 22:23:29 -0500

    Feat: Arquitectura base

---
+++ notification-socket-service/src/main/java/com/proyecto/notificacion/NotificationSocketServiceApplication.java
@@ -0,0 +1,13 @@
+package com.proyecto.notificacion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotificationSocketServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotificationSocketServiceApplication.class, args);
+	}
+
+}
