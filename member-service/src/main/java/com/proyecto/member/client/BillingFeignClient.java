009f604ba9edd58de6fe1c080fd659b742349f79
Guillermo Velásquez <102449689+guillermovez@users.noreply.github.com>
2026-06-23 16:30:07 -0500

    Actualizar BillingFeignClient.java

--- member-service/src/main/java/com/proyecto/member/client/BillingFeignClient.java
+++ member-service/src/main/java/com/proyecto/member/client/BillingFeignClient.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 
-@FeignClient(name = "billing-service", url = "http://localhost:8082")
+@FeignClient(name = "billing-service", url = "http://billing-service:8082")
 public interface BillingFeignClient {
 
 	@PostMapping("/api/billing/invoices")
